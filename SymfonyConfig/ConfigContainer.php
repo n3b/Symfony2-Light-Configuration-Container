bf1e8f0cdbd783becf47c1f585e7a48748b8d698
n3b <neb802@gmail.com>
2011-09-22 14:37:59 +0400

    refactored

---
+++ SymfonyConfig/ConfigContainer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace n3b\Bundle\Util\SymfonyConfig;
+
+class ConfigContainer implements \ArrayAccess
+{
+    private $elements;
+
+    public function get($key)
+    {
+        $keys = explode('.', $key);
+
+        if(isset($this->elements[$keys[0]])) {
+            if(count($keys) > 1 && $this->elements[$keys[0]] instanceof ConfigContainer)
+                    return $this->elements[$keys[0]]->get(\str_replace($keys[0] . '.',
+                            '', $key));
+
+            return $this->elements[$keys[0]];
+        }
+
+        return null;
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->elements[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if(!isset($offset)) {
+            return $this->add($value);
+        }
+        
+        return $this->elements[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if (isset($this->elements[$offset])) {
+            $removed = $this->elements[$offset];
+            unset($this->elements[$offset]);
+            
+            return $removed;
+        }
+
+        return null;
+    }
+    
+    public function add($value)
+    {
+        $this->elements[] = $value;
+        
+        return true;
+    }
+}
